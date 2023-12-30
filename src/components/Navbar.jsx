7be755650754652bcb280654dc7a4e844b8c0a89
Syed Rafay <rafaysyed637@gmail.com>
2023-12-30 13:53:28 +0500

    done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,179 @@
+import '../App.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <header class="header" id="header">
+
+                <div class="navigation">
+                    <div class="nav-center container d-flex">
+                        <a href="index.html" class="logo">
+                            <h1>LOGO</h1>
+                        </a>
+
+                        <ul class="nav-list d-flex">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Shop</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Contact</a>
+                            </li>
+                        </ul>
+
+                        <div class="icons d-flex">
+                            <div class="btno">
+                                <span class="d-flex"><button>Sign Up</button></span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="hero">
+                    <div class="glide" id="glide_1">
+                        <div class="glide__track" data-glide-el="track">
+                            <ul class="glide__slides">
+                                <li class="glide__slide">
+                                    <div class="center">
+                                        <div class="left">
+                                            <h1 class="">NEW Generate more leads with</h1>
+                                            <h1 class="">a professional landing page</h1>
+
+                                            <div className="mail d-flex">
+                                                <div>
+                                                    <input type="text" placeholder='Email Address...' />
+                                                    <button>Submit</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row d-flex conta">
+                    <div className="col-1">
+                        <span><i class="fa-solid fa-box-archive"></i></span>
+                        <h2>Fully Responsive</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+
+                    <div className="col-2">
+                        <span><i class="fa-solid fa-pentagon-archive"></i></span>
+                        <h2>Bootstarp 5 Ready</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+
+                    <div className="col-3">
+                        <span><i class="fa-solid fa-square-archive"></i></span>
+                        <h2>Easy to Use</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+                </div>
+
+                <div className="row d-flex hui">
+                    <div className="col-4">
+                        <h1>Fully Responsive Designs</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+
+                    <div className="col-5">
+                        <h1>Fully Responsive Designs</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+                </div>
+
+                <div className="row d-flex ohno">
+                    <div className="col-6">
+                        <h1>Fully Responsive Designs</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+
+                    <div className="col-7">
+                        <h1>Fully Responsive Designs</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+                </div>
+
+                <div className="row d-flex huya">
+                    <div className="col-8">
+                        <h1>Fully Responsive Designs</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+
+                    <div className="col-9">
+                        <h1>Fully Responsive Designs</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti pariatur possimus alias enim, qui ratione officia maxime tenetur eos quibusdam reprehenderit velit architecto fuga iusto ipsa, vitae tempore! Ex, beatae.</p>
+                    </div>
+                </div>
+
+                <div class="hero">
+                    <div class="glide" id="glide_1">
+                        <div class="glide__track" data-glide-el="track">
+                            <ul class="glide__slides">
+                                <li class="glide__slide">
+                                    <div class="center">
+                                        <div class="left">
+                                            <h1 class="">NEW Generate more leads with</h1>
+                                            <h1 class="">a professional landing page</h1>
+
+                                            <div className="mail d-flex">
+                                                <div>
+                                                    <input type="text" placeholder='Email Address...' />
+                                                    <button>Submit</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col d-flex">
+                            <h4>INFORMATION</h4>
+                            <a href="Pages/about.html">About us</a>
+                            <a href="Pages/contact.html">Contact Us</a>
+                            <a href="Pages/login.html">My Account</a>
+                        </div>
+
+                        <div class="col d-flex">
+                            <h4>USEFUL LINK</h4>
+                            <a href="Pages/faq.html">FaQs</a>
+                            <a href="Pages/terms.html">Terms & Conditions</a>
+                            <a href="Pages/privacy.html">Privacy Policy</a>
+                        </div>
+
+                        <div class="col d-flex">
+                            <h4>USEFUL LINK</h4>
+                            <a href="Pages/faq.html">FaQs</a>
+                            <a href="Pages/terms.html">Terms & Conditions</a>
+                            <a href="Pages/privacy.html">Privacy Policy</a>
+                        </div>
+                        <div class="col d-flex">
+                            <span><i class='bx bxl-facebook-square'></i></span>
+                            <span><i class='bx bxl-instagram-alt'></i></span>
+                            <span><i class='bx bxl-github'></i></span>
+                            <span><i class='bx bxl-twitter'></i></span>
+                            <span><i class='bx bxl-pinterest'></i></span>
+                        </div>
+                    </div>
+                </footer>
+
+            </header>
+        </>
+    );
+};
+
+export { Navbar };
